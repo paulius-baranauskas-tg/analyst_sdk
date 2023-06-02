3fc7aef17cde2074bb87f79ff71b00e3c6c86a72
Paulius Baranauskas <paulius.baranauskas@transfergo.com>
2023-06-02 10:18:06 +0100

    Added requirements

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 setup(
     name='tgo_analyst_sdk',
-    version='0.0.2',
+    version='0.0.3',
     description='Commonly used custom Python functions in analytical tasks.',
     packages=['tgo_analyst_sdk'],
     author='Paulius Baranauskas',
     author_email='paulius.baranauskas@transfergo.com',
-    url='https://github.com/pauliusbar/analyst_sdk'
+    url='https://github.com/pauliusbar/analyst_sdk',
+    install_requires=["dateutil", "datetime", "sklearn", "matplotlib", "boto3", "sqlalchemy", "urllib", "pandas", "sqlalchemy-redshift", "redshift_connector", "psycopg2-binary"]
 )
