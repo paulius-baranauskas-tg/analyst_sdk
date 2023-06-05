bb262a4d8020d29ab8d483508a8319f7d77ecbf9
Paulius Baranauskas <paulius.baranauskas@transfergo.com>
2023-06-05 16:26:52 +0300

    Removed version requirements for requirement list

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ setup(
     author_email='paulius.baranauskas@transfergo.com',
     url='https://github.com/pauliusbar/analyst_sdk',
     install_requires=[
-        "dateutils>=0.6.12",
-        "scikit-learn>=1.0",
-        "boto3>=1.26",
-        "sqlalchemy>=2.0",
-        "pandas>=1.5",
-        "sqlalchemy-redshift>=0.8.14",
-        "redshift_connector>=2.0.910",
-        "psycopg2-binary>=2.9.6"]
+        "dateutils>",
+        "scikit-learn",
+        "boto3",
+        "sqlalchemy",
+        "pandas",
+        "sqlalchemy-redshift",
+        "redshift_connector",
+        "psycopg2-binary"]
 )
