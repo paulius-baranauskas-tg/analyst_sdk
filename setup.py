717f0262dd8344a436c8e3274770a90f11a1987b
Paulius Baranauskas <paulius.baranauskas@transfergo.com>
2024-02-23 07:32:55 +0200

    Bumping version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tgo_analyst_sdk',
-    version='0.1.6',
+    version='0.1.7',
     description='Commonly used custom Python functions in analytical tasks.',
     packages=['tgo_analyst_sdk'],
     author='Paulius Baranauskas',
